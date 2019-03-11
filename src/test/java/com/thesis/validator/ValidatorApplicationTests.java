3f2368b2dd1f0f3eb10b218b5aff1ec9fe87bb09
Michel Cojocaru <michel.cojo@gmail.com>
2019-03-11 12:35:15 +0100

    added Spring Boot Application Stub

---
+++ src/test/java/com/thesis/validator/ValidatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thesis.validator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ValidatorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

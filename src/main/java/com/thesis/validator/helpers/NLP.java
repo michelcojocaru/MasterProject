64feef28be37f72c2229df52a46c526bffe289e0
Michel Cojocaru <michel.cojo@gmail.com>
2019-03-25 19:55:53 +0100

    added NLP logic for checking similarity between entities of the same service

--- src/main/java/com/thesis/validator/helpers/NLP.java
+++ src/main/java/com/thesis/validator/helpers/NLP.java
@@ -1,13 +1,18 @@
 package com.thesis.validator.helpers;
 
+import com.thesis.validator.model.Service;
 import edu.cmu.lti.lexical_db.ILexicalDatabase;
 import edu.cmu.lti.lexical_db.NictWordNet;
 import edu.cmu.lti.ws4j.RelatednessCalculator;
 import edu.cmu.lti.ws4j.impl.WuPalmer;
 import edu.cmu.lti.ws4j.util.WS4JConfiguration;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class NLP {
 
+    private static final double SIMILARITY_COEFFICIENT_THRESHOLD = 0.2;
     private static ILexicalDatabase db = new NictWordNet();
     private static RelatednessCalculator[] rcs = {
             /*new HirstStOnge(db), new LeacockChodorow(db),
@@ -17,10 +22,27 @@ public class NLP {
     public static double calculateSemanticSimilarity(String word1, String word2) {
         WS4JConfiguration.getInstance().setMFS(true);
         for (RelatednessCalculator rc : rcs) {
-            double s = rc.calcRelatednessOfWords(word1, word2);
-            return s;
+            return rc.calcRelatednessOfWords(word1, word2);
             //System.out.println(rc.getClass().getName() + "\t" + s);
         }
         return 0.0;
+    }
+
+    public static Boolean checkSemanticSimilarity(List<Service> services) {
+        for (Service service : services) {
+            HashSet<String> distinctEntities = Helper.getDistinctEntities(service);
+            if (distinctEntities.size() > 1) {
+                String[] entities = new String[distinctEntities.size()];
+                distinctEntities.toArray(entities);
+                for (int i = 0; i < entities.length; i++) {
+                    for (int j = i + 1; j < entities.length; j++) {
+                        if (calculateSemanticSimilarity(entities[i], entities[j]) < SIMILARITY_COEFFICIENT_THRESHOLD) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
